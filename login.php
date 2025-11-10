2151729f5681c16c3b20145d2ac995c2997a79de
andreisavu2204 <andreisavu298@gmail.com>
2025-11-10 13:39:27 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
 
+// Preluăm datele din formular
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $redirect_to = $_POST['redirect'] ?? 'management.html';
 
-// Exemplu de utilizatori (într-un proiect real folosești DB)
-$valid_users = [
-    'admin' => '1234',
-    'user' => 'password'
-];
+// Setăm sesiunea ca fiind logată, indiferent de date
+$_SESSION['loggedin'] = true;
+$_SESSION['username'] = $username ?: 'Guest';
 
-if(isset($valid_users[$username]) && $valid_users[$username] === $password){
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-
-    header("Location: $redirect_to");
-    exit;
-} else {
-    echo "<h1 style='text-align:center;'>Login eșuat</h1>";
-    echo "<p style='text-align:center;'>Username sau parolă greșită.</p>";
-    echo '<p style="text-align:center;"><a href="login.html" class="arcade-button">Înapoi</a></p>';
-}
+// Redirecționăm utilizatorul
+header("Location: $redirect_to");
+exit;
 ?>
